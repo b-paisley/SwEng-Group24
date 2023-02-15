f5f1a8a50367d6634a4c81b1329996d889c83276
lukeorourke <orourklu@tcd.ie>
2023-02-15 13:47:37 +0000

    created chess ui

---
+++ Version1/chessBoard/chessBoard.py
@@ -0,0 +1,20 @@
+class ChessBoard:
+    def __init__(self):
+        self.board = [
+            ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ["P", "P", "P", "P", "P", "P", "P", "P"],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
+            ["p", "p", "p", "p", "p", "p", "p", "p"],
+            ["r", "n", "b", "q", "k", "b", "n", "r"],
+        ]
+
+    def draw_board(self):
+        for i in range(8, 0, -1):
+            print(f"{i} |", end=" ")
+            for j in range(0, 8):
+                print(f"{self.board[i-1][j]} |", end=" ")
+            print("\n  +---+---+---+---+---+---+---+---+")
+        print("    a   b   c   d   e   f   g   h")
---
+++ Version1/chessBoard/main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from chessBoard import ChessBoard
+
+board = ChessBoard()
+board.draw_board()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
