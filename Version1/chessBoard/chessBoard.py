7afba6af7426d6fc0bf9fb95ba21d9b84fb7c4a7
lukeorourke <orourklu@tcd.ie>
2023-02-17 13:34:49 +0000

    added get board function, returning current state of the chess board

--- Version1/chessBoard/chessBoard.py
+++ Version1/chessBoard/chessBoard.py
@@ -1,5 +1,9 @@
+from Pieces import Piece
+
+
 class ChessBoard:
     def __init__(self):
+        # creates 2D 8X8 Array of pieces in their starting positions
         self.board = [
             ["R", "N", "B", "Q", "K", "B", "N", "R"],
             ["P", "P", "P", "P", "P", "P", "P", "P"],
@@ -10,10 +14,44 @@ class ChessBoard:
             ["p", "p", "p", "p", "p", "p", "p", "p"],
             ["r", "n", "b", "q", "k", "b", "n", "r"],
         ]
+        # this attribute is used to keep track of the pieces on the board. When a piece is added to the board, it also
+        # gets added to the self.pieces list. likewise when a piece is removed from the board, it is removed from the
+        # self.pieces list.
+        self.pieces = []
+        for i in range(8):
+            for j in range(8):
+                if self.board[i][j] == " ":
+                    continue
+                elif self.board[i][j] == "P":
+                    self.pieces.append(Piece(1, False, (i, j)))
+                elif self.board[i][j] == "p":
+                    self.pieces.append(Piece(1, True, (i, j)))
+                elif self.board[i][j] == "R":
+                    self.pieces.append(Piece(5, False, (i, j)))
+                elif self.board[i][j] == "r":
+                    self.pieces.append(Piece(5, True, (i, j)))
+                elif self.board[i][j] == "N":
+                    self.pieces.append(Piece(3, False, (i, j)))
+                elif self.board[i][j] == "n":
+                    self.pieces.append(Piece(3, True, (i, j)))
+                elif self.board[i][j] == "B":
+                    self.pieces.append(Piece(3, False, (i, j)))
+                elif self.board[i][j] == "b":
+                    self.pieces.append(Piece(3, True, (i, j)))
+                elif self.board[i][j] == "Q":
+                    self.pieces.append(Piece(9, False, (i, j)))
+                elif self.board[i][j] == "q":
+                    self.pieces.append(Piece(9, True, (i, j)))
+                elif self.board[i][j] == "K":
+                    self.pieces.append(Piece(200, False, (i, j)))
+                elif self.board[i][j] == "k":
+                    self.pieces.append(Piece(200, True, (i, j)))
 
+    # return current state of board
     def get_board(self):
         return self.board
 
+    # print to console a visual representation of the chess board
     def draw_board(self):
         for i in range(8, 0, -1):
             print(f"{i} |", end=" ")
@@ -23,6 +61,7 @@ class ChessBoard:
         print("    a   b   c   d   e   f   g   h")
 
 
+# take in array notation and return chess notation co-ordinates ( e.g. input : (2, 2) - output : d4
 def get_chess_notation(coords):
     columns = "abcdefgh"
     row = str(coords[1])
