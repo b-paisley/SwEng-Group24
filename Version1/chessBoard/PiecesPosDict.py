da5624c3275bd0a9af86e42272ca3e444a60dcf6
Ralf Duli <92692357+RalfDuli@users.noreply.github.com>
2023-02-21 14:54:18 +0000

    Cleaned main file

---
+++ Version1/chessBoard/PiecesPosDict.py
@@ -0,0 +1,39 @@
+from Pieces import *
+
+pieces_pos_dict = {
+    # White pieces
+    Rook(False): "A1",
+    Knight(False): "B1",
+    Bishop(False): "C1",
+    Queen(False): "D1",
+    King(False): "E1",
+    Bishop(False): "F1",
+    Knight(False): "G1",
+    Rook(False): "H1",
+    Pawn(False): "A2",
+    Pawn(False): "B2",
+    Pawn(False): "C2",
+    Pawn(False): "D2",
+    Pawn(False): "E2",
+    Pawn(False): "F2",
+    Pawn(False): "G2",
+    Pawn(False): "H2",
+
+    # Black pieces
+    Pawn(True): "A7",
+    Pawn(True): "B7",
+    Pawn(True): "C7",
+    Pawn(True): "D7",
+    Pawn(True): "E7",
+    Pawn(True): "F7",
+    Pawn(True): "G7",
+    Pawn(True): "H7",
+    Rook(True): "A8",
+    Knight(True): "B8",
+    Bishop(True): "C8",
+    Queen(True): "D8",
+    King(True): "E8",
+    Bishop(True): "F8",
+    Knight(True): "G8",
+    Rook(True): "H8"
+}
