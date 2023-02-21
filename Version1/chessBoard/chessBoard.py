6758113fbcf9fbe4e199ce85c72f2655b259314f
Harroche <119439246+Harroche@users.noreply.github.com>
2023-02-21 15:26:35 +0000

    board displays properly

--- Version1/chessBoard/chessBoard.py
+++ Version1/chessBoard/chessBoard.py
@@ -16,7 +16,7 @@ class ChessBoard:
     def orginal_draw(self,piece,square):
         letter_file = ord(square[0])-65  #doing unicode calculations, easiest way to get the first index
         number_row = int(square[1]) -1 
-        square_obj=self.board[letter_file][number_row]
+        square_obj=self.board[number_row][letter_file]
         square_obj.place_piece(piece)
 
     def draw(self): 
@@ -24,7 +24,7 @@ class ChessBoard:
         for i in range(8):
             print(" "+str(8-i)+" ",end="")
             for j in range(8):
-                piece=self.board[-(j+1)][i].placed_in_square
+                piece=self.board[abs(i-7)][j].placed_in_square
                 print("| ",end="")
                 if (piece == None):
                     print("  ",end="")
@@ -35,17 +35,19 @@ class ChessBoard:
         print("     A   B   C   D   E   F   G   H\n")
 
 
+
+
     def update_board(self,prev_square, new_square): #C1_C2
         letter_file = ord(prev_square[0])-65
         number_row = int(prev_square[1]) -1
-        piece=self.board[letter_file][number_row].move_off_square()
+        piece=self.board[number_row][letter_file].move_off_square()
 
         letter_file = ord(new_square[0])-65
         number_row = int(new_square[1]) -1
-        self.board[letter_file][number_row].place_piece(piece)
+        self.board[number_row][letter_file].place_piece(piece)
         self.draw()
 
     def access_sqaure(self,square):
         letter_file = ord(square[0])-65
         number_row = int(square[1]) -1
-        return self.board[letter_file][number_row].get_piece()         
+        return self.board[number_row][letter_file].get_piece()         
