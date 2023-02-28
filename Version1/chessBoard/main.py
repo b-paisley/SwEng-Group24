1e1f984eac4e54a8c0755f80e061456c55541a6c
BillDoolin1 <123823079+BillDoolin1@users.noreply.github.com>
2023-02-28 23:44:54 +0000

    removed my unit testing comments from main

--- Version1/chessBoard/main.py
+++ Version1/chessBoard/main.py
@@ -146,51 +146,6 @@ print(move_checker(board, "C5", "D7")) # False
 print(move_checker(board, "C5", "F8")) # False
 print(move_checker(board, "C5", "F2")) # True
 
-
-# Bill checking if King is working
-#test set #1 move king around unchecked
-board.update_board("E8", "E5")  # update the board
-print(move_checker(board, "E5", "D5")) # check if it can move left
-print(move_checker(board, "E5", "F5")) # check if it can move Right
-print(move_checker(board, "E5", "E6")) # check if it can move UP
-print(move_checker(board, "E5", "E4")) # check if it can move Down
-print(move_checker(board, "E5", "F6")) # check if it can move r U
-print(move_checker(board, "E5", "D6")) # check if it can move l u
-print(move_checker(board, "E5", "F4")) # check if it can move r d
-print(move_checker(board, "E5", "D4")) # check if it can move r d
-
-#test set #2 move king into knight checks
-
-print(move_checker(board, "E7", "E6"))  # move a pawn out of the way (true)
-board.update_board("E7", "E6")  # update the board
-print(move_checker(board, "E8", "E7"))  # move the king forward one(true)
-board.update_board("E8", "E7")  # update the board
-print(move_checker(board, "E7", "D6"))  # try to move king into open square
-board.update_board("B1", "C4")  # move knight near king
-print(move_checker(board, "E7", "D6"))  # try to move king into knight check (false)
-
-# test set #3 move king into straight checks and blocked straight checks
-
-board.update_board("E8", "E5")  # move king into the middle
-board.update_board("A1", "D3")  # move rook near middle
-print(move_checker(board, "E5", "D5")) # move king into rook check (false)
-board.update_board("D8", "D4")  # block future rook check with our queen
-print(move_checker(board, "E5", "D5")) # move king into now blocked no longer rook check (true)
-
-# test set #4 moving king into diagonal checks and blocked diagonal checks
-
-board.update_board("E8", "E5")  # move king into the middle
-board.update_board("F1", "B3")  # move bishop near king
-print(move_checker(board, "E5", "D5")) # move king into bishop check (false)
-board.update_board("D8", "C4")  # block future bishop check with our queen
-print(move_checker(board, "E5", "D5")) # move king into now blocked no longer rook check (true)
-
-# test set #5 moving king into pawn checks
-
-board.update_board("E8", "E5")  # move king into the middle
-board.update_board("F2", "C4")  # move pawn near king
-print(move_checker(board, "E5", "D5")) # move king into pawn check (false)
-
 #Testing AI Move Generator Function - black pieces
 black_pieces_array = [Knight(True,'f2',point), Pawn(True,'f2',point), Pawn(True,'d7',point)]
 print(ai_move_generator(board, black_pieces_array))
