511635a72e29130e5f4924a711642958d1ad6699
jawood25 <99403690+jawood25@users.noreply.github.com>
2023-03-08 18:00:13 +0000

    Further naming convention fixes

--- Version1/backend/static/Game.py
+++ Version1/backend/static/Game.py
@@ -12,7 +12,7 @@ class Game:
     for i in range(32):
         pieceToDraw = list(piecesPosDict.keys())[i]
         squareToFill = piecesPosDict[pieceToDraw]
-        board.orginal_draw(pieceToDraw, squareToFill)
+        board.OriginalDraw(pieceToDraw, squareToFill)
     board.draw()
     black=False
 
@@ -21,12 +21,12 @@ class Game:
 
         validMove=False #both bools have to be set false to check error handling
         properColour = False
-        validMove=moveChecker(self.board,move[0:2],move[3:5]) #if valid
-        if(self.board.access_square(move[0:2]).isBlack==self.black): #check that not moving other player piece
+        validMove=MoveChecker(self.board,move[0:2],move[3:5]) #if valid
+        if(self.board.AccessSquare(move[0:2]).isBlack==self.black): #check that not moving other player piece
             properColour=True
         if (not validMove or not properColour):
              return "error"
-        self.board.update_board(move[0:2],move[3:5]) #updates board
+        self.board.UpdateBoard(move[0:2],move[3:5]) #updates board
         self.black = not self.black #changes to other turn
 
     def GetBoard(self):
