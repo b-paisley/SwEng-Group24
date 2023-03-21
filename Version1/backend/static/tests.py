38a0471470ed89bb64bd4efcf35c4af7a46ccad5
Harroche <119439246+Harroche@users.noreply.github.com>
2023-03-21 15:12:49 +0000

    made test.py work now for api

--- Version1/backend/static/tests.py
+++ Version1/backend/static/tests.py
@@ -6,5 +6,5 @@ from app import app # Flask instance of the API
 def test_index_route():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == '{"data":"RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr"}\n'
+    assert response.data.decode('utf-8') == '{"data":{"fen":"RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr"}}\n'
 
