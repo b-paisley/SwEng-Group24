014a12df6cf391d8da2a2b50b8f4ce940ddcd489
Ralf Duli <92692357+RalfDuli@users.noreply.github.com>
2023-03-26 02:41:47 +0100

    Made function that stores PGN data into a SQL db

---
+++ Version1/backend/static/PGNToSQL.py
@@ -0,0 +1,45 @@
+import ReadPGNMoves
+import mysql.connector
+
+
+def PGNToSQL():
+    db_passwd="Chess=iS@cooL174" # TODO: Make db_passwd environment variable
+
+    # Database variable
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd=db_passwd,
+        database="pgn_database"
+    )
+
+    cursor = db.cursor()
+
+    PGNData = ReadPGNMoves.PGNArray.PGNMoves() # List of PGN games
+
+    for i in range(len(PGNData)):
+        # Create a table for each PGN game in the list
+        cursor.execute(
+        f"""CREATE TABLE Game{i}
+        (Move int PRIMARY KEY AUTO_INCREMENT,
+        WhiteMoves varchar(10),
+        BlackMoves varchar(10))
+        """)
+
+        # Insert the white and black moves into the table
+        PGNGame = PGNData[i]
+        for i in PGNGame.whitePieces:
+            if i == len(PGNGame.BlackPieces):
+                cursor.execute(
+                f"""INSERT INTO Game{i}
+                (WhiteMove. BlackMove)
+                VALUES (%s,%s)
+                """, (PGNGame.WhitePieces[i], None))
+            else:
+                cursor.execute(
+                    f"""INSERT INTO Game{i}
+                    (WhiteMove. BlackMove)
+                    VALUES (%s,%s)
+                    """, (PGNGame.WhitePieces[i], PGNGame.BlackPieces[i]))
+                
+    db.commit()
