1cd24c6793a915989174d6583392296bac6baa43
paisleyb <paisleyb@tcd.ie>
2023-04-03 23:25:51 +0100

    Another attempt

--- Version1/backend/static/NetComponents.py
+++ Version1/backend/static/NetComponents.py
@@ -72,8 +72,9 @@ class NetVal(object):
         p = os.getcwd()
         if os.path.isfile(os.path.join(p, '/nets/value.pth')):
           p = p + '/nets/value.pth'
-        elif os.path.isfile(p + '\\Version1\\backend\\static\\nets\\value.pth'): 
-          p = '\\Version1\\backend\\static\\nets\\value.pth'
+        else:
+        #os.path.isfile(p + '\\Version1\\backend\\static\\nets\\value.pth'): 
+          p = '\\nets\\value.pth'
         vals = torch.load(p, map_location=lambda storage, loc: storage)
         self.model = Net()
         self.model.load_state_dict(vals)
