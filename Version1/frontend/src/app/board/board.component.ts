76d91a639c106de05ca1ce92c24edba0bbe1c98c
Hassan Alekri <85484546+alekrih@users.noreply.github.com>
2023-04-03 17:53:05 +0100

    altered css to make it more dynamic. if fullscreen, the board size will increase to fill more space

--- Version1/frontend/src/app/board/board.component.ts
+++ Version1/frontend/src/app/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener } from "@angular/core";
 import { Coord } from "../class/coord";
 import { PiecesService } from "../services/pieces.service";
 import { FormGroup, FormControl, Validators } from '@angular/forms';
