8ea9b1d967b4dfb0c0cab4373bdd5c76c9af53f3
Ralf Duli <92692357+RalfDuli@users.noreply.github.com>
2023-04-09 16:31:37 +0100

    Allowed function to change board

--- Version1/backend/static/PawnPromotion.py
+++ Version1/backend/static/PawnPromotion.py
@@ -1,17 +1,24 @@
 from Pieces import *
 
-def PawnPromotion(currentBoard, move):
+def PawnPromotion(currentBoard, move, newPieceNotation):
+    '''
+    Function that takes in the board, the current move being
+    played and the new notation of the piece. (R,N,B,Q)
+
+    Promotes the pawn.
+    '''
     coords = GetCoords(move[3:5])
     piece = currentBoard.board[coords[0][0]][coords[0][1]].placedInSquare
     if isinstance(piece, Pawn):
        i = 0
        if piece.isBlack == True and coords[0][0] == 0:
-           promote = input("Choose a piece to promote to [Q, R, B, N]")
-           piece = Promote(piece, promote)
+           piece = Promote(piece, newPieceNotation)
+           currentBoard.board[coords[0][0]][coords[0][1]].placedInSquare = piece
            return piece
        elif piece.isBlack == False and coords[0][0] == 7:
            promote = input("Choose a piece to promote to [Q, R, B, N] ")
-           piece = Promote(piece, promote)
+           piece = Promote(piece, newPieceNotation)
+           currentBoard.board[coords[0][0]][coords[0][1]].placedInSquare = piece
            return piece
     return piece
 
